147a00ecc35673885431eb9b3a6a020c66158af0
tarunjais28 <tarunjais28@gmail.com>
2022-12-17 21:49:43 +0530

    Module for Custom Errors

---
+++ programs/multisig-transaction/src/errors.rs
@@ -0,0 +1,16 @@
+use super::*;
+
+#[error_code]
+pub enum CustomError {
+    #[msg("Not all admins voted for the proposals.")]
+    NotAllVoters,
+
+    #[msg("Minimum threshold for transaction not met.")]
+    ThresholdNotMet,
+
+    #[msg("Vote is already casted.")]
+    VoteAlreadyCasted,
+
+    #[msg("Balance is not enough.")]
+    InsufficientFunds,
+}
